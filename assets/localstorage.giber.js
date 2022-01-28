abc81c830bfbca92a81026ece429625ae9ab8aa0
Erick Ramirez <77713259+Erickgiber@users.noreply.github.com>
2022-01-28 19:52:54 -0400

    Delete assets directory

--- assets/localstorage.giber.js
+++
@@ -1,5 +0,0 @@
-const db = {
-  set: ({name, data})=> { return window.localStorage.setItem(name, data) },
-  get: name=> { return window.localStorage.getItem(name) },
-  remove: name=> { return window.localStorage.removeItem(name) }
-}
